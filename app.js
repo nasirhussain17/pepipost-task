c0020f88d09700aa4fbc6c58a7ccaa211d057e10
Md Nasir Husaain <nasirhussain17@gmail.com>
2021-03-12 16:53:34 +0530

    Sending To Multiple Recepients

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const request = require('request');
 const fs = require("fs");
 const _configuration = require("./config/config")
 const _baseUri = _configuration.BASEURI;
-const helper = require('./helper');
+const helper = require('./Helper/helper');
 const _pathUrl = '/mail/send';
 const _queryBuilder = `${_baseUri}${_pathUrl}`;
-const users = require('./users');
+const users = require('./users-data/users');
 
 
 
@@ -19,7 +19,70 @@ const _headers = {
     api_key: _configuration.apiKey,
 };
 
-var arr= [];
+var userdata =[]
+var userinfo={}
+users.map((user)=>{
+  userinfo={
+    "email":user.email,
+    "name":user.name
+  }
+  userdata.push(userinfo)
+})
+
+let body={
+  "from": {
+    "email": "info@tagnpin.com",
+    "name": "Lesley Knope"
+  },
+  "subject": "Tribute to Lil'Sebastian",
+  "template_id": 0,
+  "content": [
+    {
+      "type": "html",
+      "value": "[%LEAD%] & [%BAND%] will be singing a tribute song for our Lil'Sebastian."
+    }
+  ],
+  "personalizations": [
+    {
+      "attributes": {
+        "LEAD": "Andy Dwyer",
+        "BAND": "Mouse Rat"
+      },
+      "to": userdata,
+    }
+  ],
+}
+
+const _options = {
+  url: _queryUrl,
+  method: 'POST',
+  headers: _headers,
+  body: helper.jsonSerialize(body),
+};
+
+
+request(_options,function (error, response, body) {
+
+if (response.statusCode >= 200 && response.statusCode <= 206){
+  let r = JSON.parse(body)
+  let res = JSON.stringify(r,null,2)
+  console.log(res);
+ fs.writeFileSync('res.json',res,function(err) {
+     if (err) {
+        return console.error(err);
+     }})
+ 
+ }else{
+  let err = JSON.stringify(error);
+  fs.writeFileSync('err.json',err,function(err) {
+      if (err) {
+         return console.error(err);
+      }})
+ }
+
+});
+
+/*
 users.map((user)=>{
   let id = user.Id
   let body={
@@ -87,3 +150,4 @@ request(_options,function (error, response, body) {
 });
 })
 
+*/
